7887fc6e09af6006d85a7ae9808dd1918060a63b
NafisaHassan114 <nafisanafta469@gmail.com>
2024-03-11 17:19:29 +0300

    this js array

---
+++ main.js
@@ -0,0 +1,11 @@
+//Example of object
+const student = {name:"Abdirashid", age:29, Address:"w/nabada",
+sayHi: () => {
+    console.log("hello")
+}}
+console.log(student.name)
+console.log(student.sayHi())
+
+//Example of Array
+const persons = ["nafta","huda","Asad"]
+console.log(persons[0])
